a5f985a0b87c9bd852bcb853accfd7c31640dd58
Gökçe Merdun <agmmnn@gmail.com>
2023-02-28 20:09:44 +0300

    uh

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -1,100 +1,163 @@
-import axios from "axios";
+// https://beta.nextjs.org/docs/routing/route-handlers
+import axios, { AxiosResponse } from "axios";
 import https from "https";
 import sizeOf from "image-size";
+import Jimp from "jimp";
 
-const agent = new https.Agent({ rejectUnauthorized: true });
 const httpsAgent = new https.Agent({
   rejectUnauthorized: false,
 });
 const axiosWithSSL = axios.create({ httpsAgent });
 
-// https://beta.nextjs.org/docs/routing/route-handlers
+interface ImageDimensions {
+  width: number;
+  height: number;
+}
 
-// https://catalog.shm.ru/entity/OBJECT/2137429
+const tileSize = 512;
 
-// https://collection.pushkinmuseum.art/en/entity/OBJECT/262426
-// http://localhost:3000/api/image?url=https%3A%2F%2Fcollection.pushkinmuseum.art%2Fen%2Fentity%2FOBJECT%2F262426
-export async function GET(request: Request) {
+export async function GET(request: Request): Promise<Response> {
   const { searchParams } = new URL(request.url);
   const url = decodeURIComponent(searchParams.get("url") as string);
 
-  const response = await axiosWithSSL.get(url);
-  const html = await response.data;
-  const regex = /<meta property="og:image" content="(.*?)\?w=1000&amp;h=1000">/;
+  const response: AxiosResponse<string> = await axiosWithSSL.get(url);
+  const html: string = response.data;
+  const regex = /<meta property="og:image" content="(.*?)\?w=1000&amp;h=1000/;
   const match = html.match(regex);
   if (match) {
     const imageUrl = match[1];
+    console.log(imageUrl);
     const { width, height } = await getImageDimensions(
       imageUrl + "?w=2000&h=2000"
     );
-    const tileSize = 512;
-    const { wStep, hStep } = {
-      wStep: parseInt(String(width / tileSize)),
-      hStep: parseInt(String(height / tileSize)),
-    };
-    const { wMod, hMod } = {
-      wMod: width % tileSize ? width % tileSize : 512,
-      hMod: height % tileSize ? height % tileSize : 512,
-    };
-    let tileUrls = [];
+
+    const wStep = Math.floor(width / tileSize);
+    const hStep = Math.floor(height / tileSize);
+    const wMod = width % tileSize;
+    const hMod = height % tileSize;
+    const tileUrls: string[] = [];
+
     for (let i = 0; i < hStep + 1; i += 1) {
       for (let j = 0; j < wStep + 1; j += 1) {
-        console.log(i, j);
-        if (i === wStep && j === hStep) {
+        const cw = j === wStep ? wMod : tileSize;
+        const ch = i === hStep ? hMod : tileSize;
+        if (cw !== 0 && ch !== 0) {
           tileUrls.push(
-            imageUrl +
-              `?w=2000&h=2000&cl=${i * tileSize}&ct=${
-                j * tileSize
-              }&cw=${wMod}&ch=${hMod}`
-          );
-        } else if (i === wStep) {
-          tileUrls.push(
-            imageUrl +
-              `?w=2000&h=2000&cl=${i * tileSize}&ct=${
-                j * tileSize
-              }&cw=${wMod}&ch=512`
-          );
-        } else if (j === hStep) {
-          tileUrls.push(
-            imageUrl +
-              `?w=2000&h=2000&cl=${i * tileSize}&ct=${
-                j * tileSize
-              }&cw=512&ch=${hMod}`
-          );
-        } else {
-          tileUrls.push(
-            imageUrl +
-              `?w=2000&h=2000&cl=${i * tileSize}&ct=${
-                j * tileSize
-              }&cw=512&ch=512`
+            `${imageUrl}?w=2000&h=2000&cl=${j * tileSize}&ct=${
+              i * tileSize
+            }&cw=${cw}&ch=${ch}`
           );
         }
       }
     }
     console.log(tileUrls);
-    console.log(tileUrls.length);
 
-    return new Response(imageUrl);
+    const imageBuffer = await getMergedImage(
+      tileUrls,
+      wStep + 1,
+      hStep + 1,
+      wMod,
+      hMod
+    );
+    const headers = {
+      "Content-Type": "image/png",
+      "Cache-Control": "public, max-age=31536000, immutable",
+    };
+
+    return new Response(imageBuffer, { status: 200, headers: headers });
   } else {
     console.log("unsuccess");
-    return new Response("not found");
+    return new Response("not found", { status: 404 });
   }
 }
 
-async function getImageDimensions(
-  url: string
-): Promise<{ width: number; height: number }> {
+async function getImageDimensions(url: string): Promise<ImageDimensions> {
   try {
     const response = await axiosWithSSL.get(url, {
       responseType: "arraybuffer",
     });
     const dimensions = sizeOf(response.data);
-    return {
-      width: dimensions.width as number,
-      height: dimensions.height as number,
-    };
+    if (dimensions.width && dimensions.height) {
+      return { width: dimensions.width, height: dimensions.height };
+    } else {
+      return { width: -1, height: -1 };
+    }
   } catch (error) {
     console.error(error);
     return { width: -1, height: -1 };
   }
 }
+
+async function getMergedImage(
+  tileUrls: string[],
+  columns: number,
+  rows: number,
+  cw: number,
+  ch: number
+): Promise<Buffer> {
+  const tileImages = await Promise.all(
+    tileUrls.map(async (url) => {
+      const response = await axiosWithSSL.get(url, {
+        responseType: "arraybuffer",
+      });
+      return await Jimp.read(response.data);
+    })
+  );
+
+  const maxWidth = tileImages.reduce(
+    (max, img) => Math.max(max, img.bitmap.width),
+    0
+  );
+  const maxHeight = tileImages.reduce(
+    (max, img) => Math.max(max, img.bitmap.height),
+    0
+  );
+  const ratio = maxHeight / tileSize;
+
+  const mergedImage = await new Jimp(maxWidth * columns, maxHeight * rows);
+
+  let x = 0,
+    y = 0;
+
+  for (let i = 0; i < tileImages.length; i += 1) {
+    mergedImage.composite(tileImages[i], x, y);
+    x += maxWidth;
+    if (i % columns === columns - 1) {
+      x = 0;
+      y += maxHeight;
+    }
+  }
+
+  const croppedImage = mergedImage.crop(
+    0,
+    0,
+    (columns - 1) * maxWidth + cw * ratio,
+    (rows - 1) * maxHeight + ch * ratio
+  );
+
+  return await croppedImage.getBufferAsync(Jimp.MIME_PNG);
+}
+
+// 2000:0000 //0
+// 4000:0000 //1
+// 0000:2000 //3
+// 2000:2000
+// 4000:2000
+// 0000:4000
+// 2000:4000
+// 4000:4000
+// 0000:6000 //9  !
+// 2000:6000 //10 !
+// 4000:6000 //11 !
+// 0000:8000 //12 !
+
+// https://catalog.shm.ru/entity/OBJECT/2137429
+
+// https://collection.pushkinmuseum.art/en/entity/OBJECT/77609
+// http://localhost:3000/api/image?url=https%3A%2F%2Fcollection.pushkinmuseum.art%2Fen%2Fentity%2FOBJECT%2F77609
+
+// https://collection.pushkinmuseum.art/en/entity/OBJECT/262426
+// http://localhost:3000/api/image?url=https%3A%2F%2Fcollection.pushkinmuseum.art%2Fen%2Fentity%2FOBJECT%2F262426
+
+//https://collection.pushkinmuseum.art/cross-search?query=art
+//https://collection.pushkinmuseum.art/entity/OBJECT/787695?query=art&index=0
--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,100 @@
+"use client";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "./page.module.css";
+import { ReactElement, useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <div>
-      <div className="pt-10">
-        <h1 className="text-center text-2xl font-bold ">
-          <a href="https://en.shm.ru/" className="text-sky-200">
-            State Historical Museum
-          </a>{" "}
-          and{" "}
-          <a
-            href="https://pushkinmuseum.art/exposition_collection/index.php?lang=en"
-            className="text-sky-200"
-          >
-            The Pushkin Museum Downloader
-          </a>
-        </h1>
-      </div>
+  const [inputValue, setInputValue] = useState<string>("");
+  const [inputUrl, setInputUrl] = useState<string>("");
+  const [imgData, setImgData] = useState<Blob>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [imgUrl, setImgUrl] = useState<string>();
 
-      <div className="flex flex-wrap items-center  overflow-x-auto overflow-y-hidden py-10 justify-center">
-        <form className="w-96">
+  useEffect(() => {
+    if (inputUrl) {
+      setIsLoading(true);
+      fetch(`/api/image?url=${inputUrl}`)
+        .then((res) => res.blob())
+        .then((blob) => {
+          setImgData(blob);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          console.error(error);
+          setIsLoading(false);
+        });
+    }
+  }, [inputUrl]);
+
+  useEffect(() => {
+    if (imgData instanceof Blob) {
+      const blobUrl = URL.createObjectURL(imgData);
+      setImgUrl(blobUrl);
+      return () => {
+        URL.revokeObjectURL(blobUrl);
+        setImgData(undefined); // set imgData to undefined to release the reference to the Blob object
+      };
+    }
+  }, [imgData]);
+
+  const handleDownloadClick = () => {
+    setInputUrl(encodeURIComponent(inputValue));
+  };
+
+  const imgDOM: ReactElement = isLoading ? (
+    <svg
+      viewBox="0 0 38 38"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="#fff"
+      className="pt-6 w-20 h-20"
+    >
+      <g fill="none" fillRule="evenodd">
+        <g transform="translate(1 1)" strokeWidth={2}>
+          <circle strokeOpacity=".5" cx={18} cy={18} r={18} />
+          <path d="M36 18c0-9.94-8.06-18-18-18">
+            <animateTransform
+              attributeName="transform"
+              type="rotate"
+              from="0 18 18"
+              to="360 18 18"
+              dur="1s"
+              repeatCount="indefinite"
+            />
+          </path>
+        </g>
+      </g>
+    </svg>
+  ) : imgUrl ? (
+    <img src={imgUrl} alt="Full size image (Save as...)" />
+  ) : (
+    <img
+      src="https://images.unsplash.com/flagged/photo-1572392640988-ba48d1a74457?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXJ0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+      alt="Placeholder"
+    />
+  );
+
+  return (
+    <>
+      <div className={inter.className}>
+        <div className="pt-10">
+          <h1 className="text-center text-2xl font-bold ">
+            <a href="https://catalog.shm.ru/" className="text-amber-200">
+              State Historical Museum
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://pushkinmuseum.art/exposition_collection/index.php?lang=en"
+              className="text-amber-200"
+            >
+              The Pushkin Museum
+            </a>{" "}
+            Downloader
+          </h1>
+        </div>
+
+        <div className="flex flex-wrap  items-center  overflow-x-auto overflow-y-hidden py-10 justify-center">
           <label htmlFor="search" className="mb-2 text-sm font-medium sr-only ">
             Search
           </label>
@@ -48,23 +119,26 @@ export default function Home() {
             <input
               type="search"
               id="search"
-              className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="block xl:w-[900px] md:w-[700px] sm:w-[400px] p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Page URL"
+              onChange={(event) => setInputValue(event.target.value)}
               required
             />
             <button
               type="submit"
-              className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-white  absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:bg-gray-500"
+              onClick={handleDownloadClick}
+              disabled={isLoading}
             >
               Download Image
             </button>
           </div>
-        </form>
-      </div>
+        </div>
 
-      <section className="flex justify-center bg-slate-600 h-screen">
-        <img src="/api/image" alt="full size image to download" />
-      </section>
-    </div>
+        <section className="flex justify-center bg-slate-600 h-[700px]">
+          {imgDOM}
+        </section>
+      </div>
+    </>
   );
 }
